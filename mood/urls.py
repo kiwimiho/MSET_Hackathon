883b3a55fe6f77e2def7ee752443883a1b734356
Summer Shen <summer@Summers-MacBook-Air.local>
2023-01-28 14:10:35 -0800

    add show_mood

--- mood/urls.py
+++ mood/urls.py
@@ -5,4 +5,6 @@ app_name = 'mood'
 
 urlpatterns=[
     path('', views.index, name='index'),
+    path('show_mood/',views.show_mood,name='show_mood'),
+    path('user_mood',views.user_mood,name='user_mood'),
 ]
--- mood/views.py
+++ mood/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
 from mood.forms import WellbeingForm
+from mood.models import WellBeing
+from django.http import HttpResponseRedirect
 # Create your views here.
 
 def index(request):
     context = {}
     context['form'] = WellbeingForm()
     return render(request, 'mood/index.html', context)
+
+
+def user_mood(request):
+    form = WellbeingForm(request.POST)
+
+    if request.method == 'POST':
+        wellbeing_form = WellbeingForm(data=request.POST)
+
+        if wellbeing_form.is_valid():
+
+            # grab user_form and save to the DB
+            choice = wellbeing_form.save()
+            choice.save()
+    return HttpResponseRedirect('/mood/show_mood')
+
+def show_mood(request):
+    mood_list = WellBeing.objects.order_by('date')
+    mood_dict = {'moods': mood_list}
+    return render(request, 'mood/showmood.html',context=mood_dict)
